01ec965c708f7dfb525b7ae7c73db3d465c48a1d
Gedoix <diegoortegop@hotmail.com>
2019-06-25 17:47:35 -0400

    fixed migrations deletion script

--- delete-migrations.bash
+++ delete-migrations.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-find . -name "migrations" -exec rm -r {} \;
+for i in (Alumnos;Cursos;Evaluaciones;Evaluadores;Proyecto1;Rubricas;Usuarios) 
+do
+find $i -name "migrations" -exec rm -r {} \;
+
 rm db.sqlite3
 
 find . -name "__pycache__" -exec rm -r {} \
